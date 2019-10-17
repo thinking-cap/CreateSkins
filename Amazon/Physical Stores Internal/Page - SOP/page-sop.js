fcb6bf8b5eed9b601580c97725425af4a5b65634
Campbell Macmillan <campbell@thinkingcap.com>
2019-10-17 13:53:34 -0400

    updated skins

--- Amazon/Physical Stores Internal/Page - SOP/page-sop.js
+++ Amazon/Physical Stores Internal/Page - SOP/page-sop.js
@@ -1,54 +1,116 @@
-/// <reference path="../../JS/TCSCO_Core.js" />
-var pagecount = 0;
-var offset = 1144;
-var currentPage = 0;
 
-LOB.OnReady.add(function () {
-    Gleam.pageCount.init();
-    pagecount = LOB.pages.length - 1;
-    var windowHeight = ($(window).height() / 2) + ($('#LOBNavigation').height() / 2);
-    $('#LOBNavigation').animate({ "top": "-" + windowHeight }, "normal");
-    $('.Callout').wrapInner('<span>');
-    $('.Image').wrapInner('<span>');
-});
+LOB.extend.Runbook_Steps = function (e) {
 
-var Gleam = {
-    pages: [],
-    currentPage: null,
-    next: function () {
-        if (currentPage < pagecount - 1) {
-            Gleam.currentPage.className = 'page';
-            for (var xx = 0; xx < LOB.pages.length; xx++) {
+    console.log("start runbook steps skin-----------------------------");
 
-                LOB.pages[xx].style.marginLeft = (LOB.pages[xx].offset - offset) + 'px';
-                LOB.pages[xx].offset = (LOB.pages[xx].offset - offset);
-            }
-            currentPage++;
-            Gleam.currentPage = Gleam.pages[currentPage];
-            Gleam.currentPage.className = 'page sel';
-        }
-
-    },
-    back: function () {
-
-        if (currentPage > 0) {
-            Gleam.currentPage.className = 'page';
-            for (var xx = 0; xx < LOB.pages.length; xx++) {
-                LOB.pages[xx].style.marginLeft = (LOB.pages[xx].offset + offset) + 'px';
-                LOB.pages[xx].offset = (LOB.pages[xx].offset + offset);
-
-            }
-            currentPage--;
-            Gleam.currentPage = Gleam.pages[currentPage];
-            Gleam.currentPage.className = 'page sel';
-        }
-    },
-    pageCount: {
-        init: function (el) {
-            LOB.pagination(Gleam);
+    var Runbook_Steps = document.createElement('ol');
+    LOB.parseElements(e.children, Runbook_Steps);
+    return { element: Runbook_Steps, css: 'Runbook_Steps' };
 
 
-        }
-    }
+    console.log("end runbook steps skin-----------------------------");
 
 };
+
+LOB.extend.Step = function (e) {
+    console.log("start runbook steps Step skin-----------------------------");
+    var el = document.createElement('li');
+    LOB.parseElements(e.children, el);
+    return { element: el };
+};
+
+LOB.extend.Heading = function (e) {
+    console.log("start runbook steps Heading skin-----------------------------");
+    var el = document.createElement('h2');
+    LOB.parseElements(e.children, el);
+    return { element: el };
+};
+
+LOB.extend.Subheading = function (e) {
+    console.log("start runbook steps Subheading skin-----------------------------");
+    var el = document.createElement('h3');
+    LOB.parseElements(e.children, el);
+    return { element: el };
+};
+
+LOB.extend.Paragraph = function (e) {
+    console.log("start runbook steps Paragraph skin-----------------------------");
+    var el = document.createElement('p');
+    LOB.parseElements(e.children, el);
+    return { element: el };
+};
+
+LOB.extend.Unordered_List = function (e) {
+    console.log("start runbook steps Unordered_List skin-----------------------------");
+    var el = document.createElement('ol');
+    LOB.parseElements(e.children, el);
+    return { element: el };
+};
+
+LOB.extend.Unordered_List = function (e) {
+    console.log("start runbook steps Unordered_List skin-----------------------------");
+    var el = document.createElement('ol');
+    LOB.parseElements(e.children, el);
+    return { element: el };
+};
+
+LOB.extend.List_Item = function (e) {
+    console.log("start runbook steps List_Item skin-----------------------------");
+    var el = document.createElement('li');
+    LOB.parseElements(e.children, el);
+    return { element: el };
+
+    // LOB.parseElements(e.children,Unordered_List);
+    // return { element: Unordered_List, css: 'list_item' };
+};
+
+LOB.extend.Callout = function (e) {
+    console.log("start runbook steps Callout skin-----------------------------");
+    var el = document.createElement('div');
+    LOB.parseElements(e.children, el);
+    return { element: el, css: 'alert alert-info' };
+};
+
+// LOB.extend.Table = function(e){
+//     var el = document.createElement(table);
+//     LOB.parseElements(e.children,el);
+//     return {element : el, css: 'table table-hover table-striped'};
+// };
+
+LOB.extend.Image = function (e) {
+    //console.log("start runbook steps Image skin-----------------------------");  
+
+    var img = document.createElement('img');
+
+    var src = LOB.checkObj(e, 'Src');
+    LOB.log('***************** Custom Image Src: ' + src + ' *******************');
+    var id = src.split('.')[0];
+    var ext = '';
+    if (src.split('.').length > 1) {
+        ext = src.split('.')[1];
+        if (ext && ext != '')
+            ext = '.' + ext;
+    }
+    if (mediaPath != '')
+        img.setAttribute('file', mediaPath + '/' + src);
+    else
+        img.setAttribute('file', baseHref + '/Create/ScoEditor/DisplayMedia.aspx?id=' + id + ext + '&type=image');
+
+    return { element: img, css: 'img-responsive' };
+};
+
+LOB.extend.Video = function (e) {
+    console.log("start runbook steps Video skin-----------------------------");
+    var el = document.createElement(div);
+    LOB.parseElements(e.children, el);
+    return { element: el, css: 'embed-responsive embed-responsive-16by9' };
+    // <iframe class="embed-responsive-item" src="..."></iframe>
+};
+
+LOB.extend.Document = function (e) {
+    console.log("start runbook steps Document skin-----------------------------");
+    var el = document.createElement(div);
+    LOB.parseElements(e.children, el);
+    return { element: el, css: 'document' };
+};
+
--- Amazon/Physical Stores Internal/Runbook_Steps/runbook_steps.js
+++ Amazon/Physical Stores Internal/Runbook_Steps/runbook_steps.js
@@ -1,9 +1,15 @@
+LOB.OnReady.add(function () {
+    if(LOB.pages.length>1)
+        Template.showFirstPage();
+});
+
+
 LOB.extend.Runbook_Steps = function (e) {
 
     console.log("start runbook steps skin-----------------------------");
- 
+
     var Runbook_Steps = document.createElement('ol');
-    LOB.parseElements(e.children,Runbook_Steps);
+    LOB.parseElements(e.children, Runbook_Steps);
     return { element: Runbook_Steps, css: 'Runbook_Steps' };
 
 
@@ -11,63 +17,63 @@ LOB.extend.Runbook_Steps = function (e) {
 
 };
 
-LOB.extend.Step = function(e){
+LOB.extend.Step = function (e) {
     console.log("start runbook steps Step skin-----------------------------");
     var el = document.createElement('li');
-    LOB.parseElements(e.children,el);
-    return {element : el};
+    LOB.parseElements(e.children, el);
+    return { element: el };
 };
 
-LOB.extend.Heading = function(e){
+LOB.extend.Heading = function (e) {
     console.log("start runbook steps Heading skin-----------------------------");
     var el = document.createElement('h2');
-    LOB.parseElements(e.children,el);
-    return {element : el};
+    LOB.parseElements(e.children, el);
+    return { element: el };
 };
 
-LOB.extend.Subheading = function(e){
+LOB.extend.Subheading = function (e) {
     console.log("start runbook steps Subheading skin-----------------------------");
     var el = document.createElement('h3');
-    LOB.parseElements(e.children,el);
-    return {element : el};
+    LOB.parseElements(e.children, el);
+    return { element: el };
 };
 
-LOB.extend.Paragraph = function(e){
+LOB.extend.Paragraph = function (e) {
     console.log("start runbook steps Paragraph skin-----------------------------");
     var el = document.createElement('p');
-    LOB.parseElements(e.children,el);
-    return {element : el};
+    LOB.parseElements(e.children, el);
+    return { element: el };
 };
 
-LOB.extend.Unordered_List = function(e){
+LOB.extend.Unordered_List = function (e) {
     console.log("start runbook steps Unordered_List skin-----------------------------");
     var el = document.createElement('ol');
-    LOB.parseElements(e.children,el);
-    return {element : el};
+    LOB.parseElements(e.children, el);
+    return { element: el };
 };
 
-LOB.extend.Unordered_List = function(e){
+LOB.extend.Unordered_List = function (e) {
     console.log("start runbook steps Unordered_List skin-----------------------------");
     var el = document.createElement('ol');
-    LOB.parseElements(e.children,el);
-    return {element : el};
+    LOB.parseElements(e.children, el);
+    return { element: el };
 };
 
-LOB.extend.List_Item = function(e){
+LOB.extend.List_Item = function (e) {
     console.log("start runbook steps List_Item skin-----------------------------");
     var el = document.createElement('li');
-    LOB.parseElements(e.children,el);
-    return {element : el};
+    LOB.parseElements(e.children, el);
+    return { element: el };
 
     // LOB.parseElements(e.children,Unordered_List);
     // return { element: Unordered_List, css: 'list_item' };
 };
 
-LOB.extend.Callout = function(e){
+LOB.extend.Callout = function (e) {
     console.log("start runbook steps Callout skin-----------------------------");
     var el = document.createElement('div');
-    LOB.parseElements(e.children,el);
-    return {element : el, css: 'alert alert-info'};
+    LOB.parseElements(e.children, el);
+    return { element: el, css: 'alert alert-info' };
 };
 
 // LOB.extend.Table = function(e){
@@ -76,25 +82,44 @@ LOB.extend.Callout = function(e){
 //     return {element : el, css: 'table table-hover table-striped'};
 // };
 
-LOB.extend.Image = function(e){
-    console.log("start runbook steps Image skin-----------------------------");
-    var el = document.createElement(img);
-    LOB.parseElements(e.children,el);
-    return {element : el, css: 'img-responsive'};
+LOB.extend.Image = function (e) {
+    //console.log("start runbook steps Image skin-----------------------------");  
+
+    var img = document.createElement('img');
+
+    var src = LOB.checkObj(e, 'Src');
+    LOB.log('***************** Custom Image Src: ' + src + ' *******************');
+    var id = src.split('.')[0];
+    var ext = '';
+    if (src.split('.').length > 1) {
+        ext = src.split('.')[1];
+        if (ext && ext != '')
+            ext = '.' + ext;
+    }
+    if (mediaPath != '')
+        img.setAttribute('file', mediaPath + '/' + src);
+    else
+        img.setAttribute('file', baseHref + '/Create/ScoEditor/DisplayMedia.aspx?id=' + id + ext + '&type=image');
+
+    return { element: img, css: 'img-responsive' };
 };
 
-LOB.extend.Video = function(e){
+LOB.extend.Video = function (e) {
     console.log("start runbook steps Video skin-----------------------------");
     var el = document.createElement(div);
-    LOB.parseElements(e.children,el);
-    return {element : el, css: 'embed-responsive embed-responsive-16by9'};
+    LOB.parseElements(e.children, el);
+    return { element: el, css: 'embed-responsive embed-responsive-16by9' };
     // <iframe class="embed-responsive-item" src="..."></iframe>
 };
 
-LOB.extend.Document = function(e){
+LOB.extend.Document = function (e) {
     console.log("start runbook steps Document skin-----------------------------");
     var el = document.createElement(div);
-    LOB.parseElements(e.children,el);
-    return {element : el, css: 'document'};
+    LOB.parseElements(e.children, el);
+    return { element: el, css: 'document' };
 };
+
+
+
+
 
